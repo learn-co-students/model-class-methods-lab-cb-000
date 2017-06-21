477d0016e3975210b85485054a4ebe24e4468549
jwald1 <email@example.com>
2017-06-21 15:13:26 -0400

    captain_spec passes

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,30 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    with_classifications.
+      where(classifications: {name: 'Catamaran'}).
+      distinct
+  end
+
+  def self.sailors
+    with_classifications.
+      where(classifications: {name: 'Sailboat'}).
+      distinct
+  end
+
+  def self.talented_seamen
+    with_classifications.
+      where(classifications: {name: %w(Sailboat Motorboat)}).
+      group('captains.name').
+      having('count(DISTINCT classifications.name) >= 2')
+  end
+
+  def self.non_sailors
+    where.not(name: sailors.pluck(:name))
+  end
+
+  def self.with_classifications
+    joins(boats: { boat_classifications: :classification })
+  end
 end
