3d84fc80eba569aee51884d11a0748850d0ca02d
Tyler Bristol <tahbristol@gmail.com>
2017-08-07 18:09:33 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -15,6 +15,10 @@ class Boat < ActiveRecord::Base
     self.where("length > ?", 20)
   end
 
+  def self.longest
+        self.where("length = ?", self.maximum("length"))
+  end
+
   def self.last_three_alphabetically
 
     self.order("name").reverse_order.limit(3)
@@ -23,4 +27,13 @@ class Boat < ActiveRecord::Base
   def self.without_a_captain
     self.where("captain_id IS NULL")
   end
+
+  def self.sailboats
+    self.joins("inner join boat_classifications on boat_classifications.boat_id = boats.id inner join classifications on classifications.id = boat_classifications.classification_id where classifications.name = 'Sailboat'")
+  end
+
+  def self.with_three_classifications
+    self.joins("inner join boat_classifications on boat_classifications.boat_id = boats.id inner join classifications on classifications.id = boat_classifications.classification_id group by boats.name having count(boat_classifications.classification_id) = 3")
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,38 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+  def self.sailors
+    self.joins("inner join boats on boats.captain_id = captains.id inner join boat_classifications on boat_classifications.boat_id = boats.id inner join classifications on classifications.id = boat_classifications.classification_id where classifications.name = 'Sailboat'").uniq
+  end
+
+  def self.catamaran_operators
+    self.joins("inner join boats on boats.captain_id = captains.id inner join boat_classifications on boat_classifications.boat_id = boats.id inner join classifications on classifications.id = boat_classifications.classification_id where classifications.name = 'Catamaran'").uniq
+
+  end
+
+  def self.motorboats
+    self.joins("inner join boats on boats.captain_id = captains.id inner join boat_classifications on boat_classifications.boat_id = boats.id inner join classifications on classifications.id = boat_classifications.classification_id where classifications.name = 'Motorboat'").uniq
+
+  end
+
+
+  def self.non_sailors
+    self.where.not(name: self.sailors_array)
+  end
+
+  def self.talented_seamen
+    self.where(name: self.sailors_array).where(name: self.motorboats_array)
+  end
+
+  def self.sailors_array
+    array = self.sailors
+    sailors = array.collect {|s| s.name}
+  end
+
+  def self.motorboats_array
+    array = self.motorboats
+    motorboaters = array.collect {|m| m.name}
+  end
+
 end
