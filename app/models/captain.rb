2a44f3ae28cc5ed62def2b1c478faf44d73fb0e1
Mariel Segovia <msegovia94@gmail.com>
2017-09-12 23:31:43 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,29 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #boat classification = catamaran
+    includes(boats: [:classifications]).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    #return a boat with a classifications of a sailboat
+    includes(boats: [:classifications]).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboaters
+    includes(boats: [:classifications]).where(classifications: {name: "Motorboat"}).uniq
+  end
+
+  def self.talented_seamen
+    #returns captains of motorboats and sailboats
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    #returns captains who are not sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
+
+
 end
