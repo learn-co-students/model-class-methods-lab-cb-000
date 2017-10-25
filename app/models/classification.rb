a292cb12f2385c35a68810f2741a7c67f6173a73
williamhancharek <william.hancharek@gmail.com>
2017-10-24 22:26:33 -0700

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,18 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  has_many :captains, through: :boats
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    a = Boat.all.select(:id).order(length: "DESC").limit(1)
+    b = self.all.select do |c|
+      c.boats.ids.include?(a.ids[0])
+    end
+    c = Classification.where(id: b.map(&:id))
+  end
+
 end
