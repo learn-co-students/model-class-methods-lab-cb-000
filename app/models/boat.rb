04bec848f07073e31c13b33feb937a6779370164
Heather Christman <efl7a@yahoo.de>
2018-02-16 17:17:20 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,42 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    self.where('length <?', 20)
+  end
+
+  def self.ship
+    self.where("length >=?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.order('name DESC').limit(3)
+  end
+
+  def self.without_a_captain
+    self.where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+    self.joins(:classifications).where(:classifications => {:name => "Sailboat"})
+  end
+
+  def self.with_three_classifications
+    with_three = []
+    self.all.each do |boat|
+      if boat.classifications.count >= 3
+        with_three << boat.id
+      end
+    end
+    self.where(id: with_three)
+  end
+
+  def self.longest
+    self.order("length DESC").first
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: :classifications).where(:classifications => {:name => "Catamaran"}).uniq
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).where(:classifications => {:name => "Sailboat"}).uniq
+  end
+
+  def self.motorboat_operators
+    self.joins(boats: :classifications).where(:classifications => {:name => "Motorboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    talented_seafarers = self.sailors & self.motorboat_operators
+    self.where(id: talented_seafarers)
+  end
+
+  def self.non_sailors
+    self.where.not(id: self.sailors)
+  end
+
 end
