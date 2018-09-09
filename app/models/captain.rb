d717e385c15ff1d908df1b83b2235b773e70e7de
Quinn Dizon <qdizon@gmail.com>
2018-09-09 17:01:20 +0000

    working

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,12 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    classification_id = Classification.find_by(name: "Catamaran").id
+    boats = Boat.all.select do |boat|
+      boat.classification_ids
+    end
+    # Boat.find_by(classifications: "")
+  end
+
 end
