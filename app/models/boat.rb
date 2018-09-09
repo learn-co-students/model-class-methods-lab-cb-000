de91b64d04a536e1aa15aba7794b0762d7b6b802
Quinn Dizon <qdizon@gmail.com>
2018-09-09 18:53:41 +0000

    working

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,11 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.catamarans
+    cat = Classification.find_by(name: "Catamaran")
+    self.all.select do |boat|
+      boat.classifications.include?(cat)
+    end
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,11 +2,24 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    classification_id = Classification.find_by(name: "Catamaran").id
-    boats = Boat.all.select do |boat|
-      boat.classification_ids
-    end
-    # Boat.find_by(classifications: "")
+    includes(boats: "classifications").where(classifications: {name: "Catamaran"}).uniq
   end
+
+  def self.sailors
+    includes(boats: "classifications").where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboat_operators
+    includes(boats: "classifications").where(classifications: {name: "Motorboat"})
+  end
+
+  def self.talented_seafarers
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
+
 
 end
