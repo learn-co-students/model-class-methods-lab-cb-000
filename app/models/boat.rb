edb897114d4a5de8eabeeeafcdf09e45383f045b
Quinn Dizon <qdizon@gmail.com>
2018-09-09 19:13:14 +0000

    working

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,10 +3,32 @@ class Boat < ActiveRecord::Base
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
 
-  def self.catamarans
-    cat = Classification.find_by(name: "Catamaran")
-    self.all.select do |boat|
-      boat.classifications.include?(cat)
-    end
+  def self.first_five
+    all.limit(5)
   end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  end
+
 end
