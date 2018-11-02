23a3659db588549c389fff44335d9702517d19a7
Adam Urbina <adamurbina@gmail.com>
2018-11-02 19:24:36 +0000

    Done.

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,11 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.classification_counts(num)
+    boat_counts = self.group(:boat_id).count
+    matching_ids = boat_counts.select{|boat_id, boat_count| boat_count == num}
+    matching_ids.keys
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.select(:name).distinct
+  end
+
+  def self.longest
+    self.joins(:boats).where(boats: {length: Boat.maximum(:length)})
+  end
+
+
 end
