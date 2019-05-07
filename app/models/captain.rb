f11fe2511b9ba1688d2b345d11fbd6382fd8c469
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-07 19:25:37 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = 'Catamaran'").distinct
+  end
+
+  def self.sailors
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = 'Sailboat'").distinct
+  end
+
+  def self.talented_seafarers
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = 'Motorboat'").where(id: self.sailors).distinct
+  end
+
+
+  def self.non_sailors
+    self.joins(boats: {boat_classifications: :classification}).where.not(id: self.sailors).distinct
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    longest_boat_id = self.joins(boat_classifications: :boat).order("boats.length desc").select("boats.id").pluck("boats.id").first
+    self.joins(:boat_classifications).where("boat_classifications.boat_id = ?", longest_boat_id)
+  end
 end
